7c479ea0461b7c232d25e2d6dd48bbdb73f3862c
nicholas <ndibellohitta1326@sdsu.edu>
2025-06-03 08:35:26 -0600

    Added vim-tmux-navigator

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,12 +1,9 @@
--- configure plugins
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {}) -- Sets Ctrl+P to "find_files"
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {}) -- Sets Space+f+g to "live_grep"
-vim.keymap.set('n', 'K', vim.lsp.buf.hover, {}) -- Sets K to show "hover" information
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {}) -- sets g+d to go to definition
-
 -- load plugins
 return {
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
   {
     "stevearc/conform.nvim",
     event = 'BufWritePre',
@@ -21,5 +18,5 @@ return {
   },
 
   require ("plugins.treesitter"),
-  require("plugins.telescope"),
+  require ("plugins.telescope"),
 }

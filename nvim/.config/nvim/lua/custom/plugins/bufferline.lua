f5be2cd3fb617d24b37bf14a62e1322a5c0e7539
NicholasMDH <n.dibellohitta@gmail.com>
2026-01-09 20:28:42 -0700

    BIG CHANGE: Switched from nvchad to my kickstart.nvim from work

---
+++ nvim/.config/nvim/lua/custom/plugins/bufferline.lua
@@ -0,0 +1,27 @@
+return {
+  'akinsho/bufferline.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+  lazy = false,
+  config = function()
+    require('bufferline').setup {
+      options = {
+        offsets = {
+          {
+            filetype = 'neo-tree',
+            text = 'filesystem',
+            highlight = 'Directory',
+            separator = true,
+          },
+        },
+      },
+    }
+
+    -- Custom keymaps
+    vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', { desc = 'Next buffer' })
+    vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', { desc = 'Previous buffer' })
+    vim.keymap.set('n', '<C-w>c', '<Cmd>bdelete<CR>', { desc = 'Close current buffer' })
+  end,
+}
